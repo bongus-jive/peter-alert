11a085b68e721f780042c1387d2f5b81f5b29599
patman <27874300+patmanf@users.noreply.github.com>
2021-07-04 20:42:00 -0500

    initial peter

---
+++ pat/peteralert/peteralert.lua
@@ -0,0 +1,48 @@
+function init()
+	Canvas = widget.bindCanvas("ok")
+	CanvasSize = Canvas:size()
+	
+	draw()
+	
+	if not config.getParameter("reopened") then
+		pane.playSound("/pat/peteralert/open.ogg", 0, 0.75)
+	end
+end
+
+function draw(pressed)
+	Canvas:clear()
+	Canvas:drawImage("/pat/peteralert/button"..(pressed and "_pressed.png" or ".png"), {0, 0}, 0.5)
+end
+
+function click(position, button, isButtonDown)
+	if button ~= 0 then return end
+	
+	local inButton = position[1] > 0 and position[1] <= CanvasSize[1] and position[2] > 0 and position[2] <= CanvasSize[2]
+	
+	if isButtonDown and inButton then
+		pane.playSound("/pat/peteralert/click.ogg", 0, 0.75)
+	end
+	
+	if not isButtonDown or inButton then
+		draw(isButtonDown)
+		
+		if not isButtonDown and lastButtonDown and inButton then
+			CLOSED = true
+			pane.dismiss()
+			sb.logInfo("%s, %s, %s", isButtonDown, lastButtonDown, inButton)
+		end
+		
+		lastButtonDown = isButtonDown
+	end
+	
+	lastPosition = position
+end
+
+function uninit()
+	if not CLOSED then
+		local peter = root.assetJson("/pat/peteralert/peteralert.config")
+		peter.gui.panefeature.offset = config.getParameter("gui").panefeature.offset
+		peter.reopened = true
+		player.interact("ScriptPane", peter)
+	end
+end
---
+++ pat/peteralert/peteralert_player.lua
@@ -0,0 +1,31 @@
+local oldInit = init or function() end
+local oldUpdate = update or function() end
+
+local peterAlerts = math.random(1, 8)
+local peterTimer = math.random(60, 120)
+
+local function peterAlert()
+	peterAlerts = (peterAlerts + 1) % 8
+	local h = 8 * peterAlerts
+	
+	local peter = root.assetJson("/pat/peteralert/peteralert.config")
+	peter.gui.panefeature.offset = {24 + h, -40 - h}
+	player.interact("ScriptPane", peter)
+end
+
+function init()
+	oldInit()
+	message.setHandler("PeterAlert.exe", peterAlert)
+end
+
+function update(dt)
+	oldUpdate(dt)
+	
+	peterTimer = math.max(0, peterTimer - dt)
+	if peterTimer == 0 then
+		peterTimer = math.random(60, 120)
+		if math.random(1, 3) == 1 then
+			peterAlert()
+		end
+	end
+end
