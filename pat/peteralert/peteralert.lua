b3e21774d86c17190d5911c9689bd3afb1743e0a
patman <27874300+patmanf@users.noreply.github.com>
2025-09-02 20:25:06 -0500

    rewrote it because im bored

--- pat/peteralert/peteralert.lua
+++ pat/peteralert/peteralert.lua
@@ -1,79 +1,62 @@
 function init()
-	Canvas = widget.bindCanvas("button")
-	CanvasSize = Canvas:size()
+  Canvas = widget.bindCanvas("button")
+  CanvasSize = Canvas:size()
 
-	BoxScale = config.getParameter("gui").box.scale or 1
-	ButtonImages = config.getParameter("buttonImages")
-	Sounds = config.getParameter("sounds")
+  WindowScale = config.getParameter("gui.window.scale", 1)
+  ButtonImages = config.getParameter("buttonImages")
+  Sounds = config.getParameter("sounds")
 
-	setPressed(false)
+  setPressed(false)
 
-	if not config.getParameter("_reopened") and not getPaneData("playedOpenSound") then
-		playSound("open")
-		setPaneData("playedOpenSound", true)
-	end
+  if not widget.getData("_opened") then
+    widget.setData("_opened", true)
+    playSound("open")
+  end
 end
 
 function click(mousePos, mouseButton, buttonDown)
-	if mouseButton ~= 0 then
-		return
-	end
-	
-	if mousePos[1] <= 0 or mousePos[1] > CanvasSize[1]
-	or mousePos[2] <= 0 or mousePos[2] > CanvasSize[2] then
-		if ButtonPressing then
-			setPressed()
-		end
-		return
-	end
-	
-	if buttonDown then
-		setPressed(true)
-		playSound("click")
-	elseif ButtonPressing then
-		setPressed(false)
-		reopen = n
-		pane.dismiss()
-	end
+  if mouseButton ~= 0 then return end
+
+  if mousePos[1] <= 0 or mousePos[1] > CanvasSize[1]
+  or mousePos[2] <= 0 or mousePos[2] > CanvasSize[2] then
+    if IsPressed then setPressed(false) end
+    return
+  end
+  
+  if buttonDown then
+    playSound("click")
+  elseif IsPressed then
+    CanDismiss = true
+    pane.dismiss()
+  end
+  
+  setPressed(buttonDown)
 end
 
 function setPressed(pressed)
-	ButtonPressing = pressed
-	drawButton(pressed)
+  IsPressed = pressed
+  drawButton()
 end
 
-function drawButton(pressed)
-	local img = ButtonImages[pressed and "pressed" or "default"]
-	Canvas:clear()
-	Canvas:drawImage(img, {0, 0}, BoxScale)
+function drawButton()
+  local img = ButtonImages[IsPressed and "pressed" or "default"]
+  Canvas:clear()
+  Canvas:drawImage(img, { 0, 0 }, WindowScale)
 end
 
 function playSound(key)
-	local sound = Sounds[key]
-	if sound then
-		pane.playSound(sound.file, 0, sound.volume)
-	end
+  local sound = Sounds[key]
+  if sound then
+    pane.playSound(sound.file, 0, sound.volume)
+  end
 end
 
-function dismissed()
-	if type(reopen) == "function" then
-		reopen()
-	end
+function uninit()
+  if not CanDismiss then return reopen() end
 end
 
 function reopen()
-	local peter = config.getParameter("")
-	peter._reopened = true
-	player.interact("ScriptPane", peter)
-end
-
-function getPaneData(key)
-	local data = widget.getData("_data") or {}
-	return data[key]
-end
-
-function setPaneData(key, value)
-	local data = widget.getData("_data") or {}
-	data[key] = value
-	widget.setData("_data", data)
+  local peter = config.getParameter("")
+  peter.gui._opened.data = true
+  player.interact("ScriptPane", peter)
 end
