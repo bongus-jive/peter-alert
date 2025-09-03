b8adb27916304440af9ee0ce17aa083fc308f03e
patman <27874300+patmanf@users.noreply.github.com>
2025-09-02 20:25:17 -0500

    why are there tabs

--- pat/peteralert/peteralert_player.lua
+++ pat/peteralert/peteralert_player.lua
@@ -1,38 +1,39 @@
 function init()
-	PaneConfig = root.assetJson("/pat/peteralert/peteralert.config")
-	Position = PaneConfig.gui.panefeature.offset
-	Stack = math.random(PaneConfig.stackMax)
+  PaneConfig = root.assetJson("/pat/peteralert/peteralert.config")
+  Position = PaneConfig.gui.panefeature.offset
+  Stack = math.random(PaneConfig.stackMax)
 
-	local minTime, maxTime = table.unpack(PaneConfig.timerRange)
-	function randomTimer() return math.random(minTime, maxTime) end
-	Timer = randomTimer()
+  local minTime, maxTime = table.unpack(PaneConfig.timerRange)
+  function randomTimer() return math.random(minTime, maxTime) end
 
-	script.setUpdateDelta(PaneConfig.playerScriptDelta or 150)
-	message.setHandler("PeterAlert.exe", peterAlert)
+  Timer = randomTimer()
+
+  script.setUpdateDelta(PaneConfig.playerScriptDelta or 150)
+  message.setHandler("PeterAlert.exe", peterAlert)
 end
 
 function update(dt)
-	Timer = Timer - dt
-	if Timer <= 0 then
-		Timer = randomTimer()
+  Timer = Timer - dt
+  if Timer <= 0 then
+    Timer = randomTimer()
 
-		if math.random() <= PaneConfig.alertChance then
-			peterAlert()
-		end
-	end
+    if math.random() <= PaneConfig.alertChance then
+      peterAlert()
+    end
+  end
 end
 
 function peterAlert()
-	Stack = (Stack + 1) % PaneConfig.stackMax
+  Stack = (Stack + 1) % PaneConfig.stackMax
 
-	PaneConfig.gui.panefeature.offset = {
-		Position[1] + (PaneConfig.stackOffset[1] * Stack),
-		Position[2] + (PaneConfig.stackOffset[2] * Stack),
-	}
+  PaneConfig.gui.panefeature.offset = {
+    Position[1] + (PaneConfig.stackOffset[1] * Stack),
+    Position[2] + (PaneConfig.stackOffset[2] * Stack),
+  }
 
-	player.interact("ScriptPane", PaneConfig)
+  player.interact("ScriptPane", PaneConfig)
 
-	if window and window.flash then
-		pcall(function() window.flash("briefly") end)
-	end
+  if window and window.flash then
+    pcall(window.flash, "briefly")
+  end
 end
